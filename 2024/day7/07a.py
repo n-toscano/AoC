9b517e1b9ec5cd45cb71e18c6b0535d0b9407c86
Nicolo' Toscano <nicolo.toscano@pasqal.com>
2024-12-07 19:03:22 +0100

    add day 7

---
+++ 2024/day7/07a.py
@@ -0,0 +1,52 @@
+from itertools import product
+
+datafolder = "data"
+with open(f"{datafolder}/07", "r") as file:
+    data = file.read()[:-1].split("\n")
+
+
+def get_result_and_terms(line):
+    return line.split(":")
+
+
+def get_operations(terms, ops=["+", "*"]):
+    return list(product(ops, repeat=(len(terms) - 1)))
+
+
+def check_combinations(terms, permutations, target_result):
+    for permutation in permutations:
+        permutation = list(permutation)
+        result = terms[0]
+        for i in range(len(permutation)):
+            result = terms[0]
+            for i, op in enumerate(permutation):
+                if op == "+":
+                    result += terms[i + 1]
+                elif op == "*":
+                    result *= terms[i + 1]
+                else:
+                    raise ValueError("Operation must be either 'add' or 'mul'")
+        if result == target_result:
+            return result
+
+    return 0
+
+
+def check_correct(line):
+    split_line = get_result_and_terms(line)
+    target_result = int(split_line[0])
+    terms = list(map(int, split_line[1].split(" ")[1:]))
+
+    permutations = get_operations(terms)
+    result = check_combinations(terms, permutations, target_result)
+
+    return result
+
+
+tot = 0
+for line in data:
+    print(line)
+    result = check_correct(line)
+    tot += result
+
+print(tot)
