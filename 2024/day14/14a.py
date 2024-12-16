899c31d85864826aae84e515b17112a3d2de6276
Nicolò Toscano <nicolo.ax@gmail.com>
2024-12-16 16:10:04 +0100

    add day 14

---
+++ 2024/day14/14a.py
@@ -0,0 +1,43 @@
+import re
+
+
+datafolder = "data"
+with open(f"{datafolder}/14", "r") as file:
+    data = file.read()[:-1].split("\n")
+
+n_rows = 101
+n_cols = 103
+
+
+def get_pos_vel(robot):
+    pattern = r"p=(\d+,\d+)\s+v=(-?\d+,-?\d+)"
+    match = re.search(pattern, robot)
+    p_tuple = tuple(map(int, match.group(1).split(",")))
+    v_tuple = tuple(map(int, match.group(2).split(",")))
+    return p_tuple, v_tuple
+
+
+def update_pos(pos, vel, i=0):
+    while i < 100:
+        new_pos_x = (pos[0] + vel[0]) % n_rows
+        new_pos_y = (pos[1] + vel[1]) % n_cols
+        new_pos = (new_pos_x, new_pos_y)
+        i += 1
+        return update_pos(new_pos, vel, i)
+    return pos
+
+
+tl, tr, bl, br = 0, 0, 0, 0
+for robot in data:
+    p, v = get_pos_vel(robot)
+    final_pos = update_pos(p, v)
+    if final_pos[0] < (n_rows - 1) / 2 and final_pos[1] < (n_cols - 1) / 2:
+        tl += 1
+    elif final_pos[0] > (n_rows) / 2 and final_pos[1] < (n_cols - 1) / 2:
+        tr += 1
+    elif final_pos[0] < (n_rows - 1) / 2 and final_pos[1] > (n_cols) / 2:
+        bl += 1
+    elif final_pos[0] > (n_rows) / 2 and final_pos[1] > (n_cols) / 2:
+        br += 1
+
+print(tl * tr * bl * br)
---
+++ 2024/day14/14b.py
@@ -0,0 +1,41 @@
+import re
+
+
+datafolder = "data"
+with open(f"{datafolder}/14", "r") as file:
+    data = file.read()[:-1].split("\n")
+
+n_rows = 101
+n_cols = 103
+
+
+def get_pos_vel(robot):
+    pattern = r"p=(\d+,\d+)\s+v=(-?\d+,-?\d+)"
+    match = re.search(pattern, robot)
+    p_tuple = tuple(map(int, match.group(1).split(",")))
+    v_tuple = tuple(map(int, match.group(2).split(",")))
+    return p_tuple, v_tuple
+
+
+def update_pos(pos, vel, i=0):
+    new_pos_x = (pos[0] + vel[0]) % n_rows
+    new_pos_y = (pos[1] + vel[1]) % n_cols
+    return (new_pos_x, new_pos_y)
+
+
+tree = False
+cnt = 0
+
+while not tree:
+    tiles_list = []
+    for robot in data:
+        p, v = get_pos_vel(robot)
+        new_pos = update_pos(p, v)
+        tiles_list.append(new_pos)
+
+    cnt += 1
+    tiles = set(tiles_list)
+    tree = True if len(tiles) == len(tiles_list) else False
+    print(cnt, end="\r")
+
+print(cnt)
